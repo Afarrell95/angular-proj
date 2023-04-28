01826b8127a01be7a75c94847f4f8ab9ba7abe22
Allison <farrella95@gmail.com>
2023-04-28 12:53:20 -0400

    things and such

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('sample recipe', 'test','https://qph.cf2.quoracdn.net/main-qimg-4ec13e3cb9f15e30d0a4ac3524facd06-lq' )
+  ];
 
   constructor() { }
 
